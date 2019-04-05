caf2866eaec82b5d84b7360fd4b0dc74ff9de061
Garrett Powell <garrett@gpowell.net>
2019-04-05 17:35:39 -0400

    Split API and application into separate modules

---
+++ api/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm")
+}
+
+version = "0.1.0"
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(group = "commons-codec", name = "commons-codec", version = "1.12")
+    implementation(group = "commons-io", name = "commons-io", version = "2.6")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "reversion"
+include("application", "api")
 
