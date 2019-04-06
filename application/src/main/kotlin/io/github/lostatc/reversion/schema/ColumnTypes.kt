dd47b816bf1dd9addf767d81c421cb883935edb7
Wren Powell <wrentpowell@gmail.com>
2019-04-05 20:56:19 -0400

    Fix copyright statements

--- application/src/main/kotlin/io/github/lostatc/reversion/schema/ColumnTypes.kt
+++ application/src/main/kotlin/io/github/lostatc/reversion/schema/ColumnTypes.kt
@@ -1,20 +1,20 @@
 /*
  * Copyright © 2019 Wren Powell
  *
- * This file is part of reversion.
+ * This file is part of Reversion.
  *
- * reversion is free software: you can redistribute it and/or modify
+ * Reversion is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * reversion is distributed in the hope that it will be useful,
+ * Reversion is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with reversion.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Reversion.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package io.github.lostatc.reversion.schema
