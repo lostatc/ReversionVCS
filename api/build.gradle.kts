c85bfccc3b777f34c089c3cc77307529e3be45af
Garrett Powell <garrett@gpowell.net>
2019-04-09 14:57:55 -0400

    Fix issue with reflection

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,6 +6,7 @@ version = "0.1.0"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
     implementation(group = "commons-codec", name = "commons-codec", version = "1.12")
     implementation(group = "commons-io", name = "commons-io", version = "2.6")
 }
