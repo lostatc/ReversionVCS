bf01ef53baf05a3cd1adfcc631f8061328e90da8
Garrett Powell <garrett@gpowell.net>
2019-04-15 15:20:33 -0400

    Move logging to SLF4J
    The purpose of this change is to allow implementations of the storage
    provider to log while keeping the application free to switch logging
    frameworks in the future. This change also allows the application's log
    files to capture logs from its dependencies.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 version = "0.1.0"
 
 dependencies {
+    api(group = "org.slf4j", name = "slf4j-api", version = "1.7.26")
     implementation(kotlin("stdlib-jdk8"))
     implementation(group = "commons-codec", name = "commons-codec", version = "1.12")
     implementation(group = "commons-io", name = "commons-io", version = "2.6")
