1d1d505c0139e09d7557f40ad53596e1d808a53e
Wren Powell <wrentpowell@gmail.com>
2019-04-27 12:02:11 -0400

    Implement equals() and hashCode() in class

--- api/src/main/kotlin/io/github/lostatc/reversion/api/Blob.kt
+++ api/src/main/kotlin/io/github/lostatc/reversion/api/Blob.kt
@@ -27,7 +27,8 @@ import java.nio.channels.Channels
 import java.nio.file.Files
 import java.nio.file.Path
 import java.security.MessageDigest
-import java.util.*
+import java.util.Enumeration
+import java.util.Objects
 
 /**
  * Returns an [Enumeration] for iterating over the contents of a [Sequence].
@@ -38,6 +39,23 @@ private fun <T> Sequence<T>.asEnumeration(): Enumeration<T> = object : Enumerati
     override fun hasMoreElements(): Boolean = iterator.hasNext()
 
     override fun nextElement(): T = iterator.next()
+}
+
+/**
+ * An abstract base class for [Blob] implementations.
+ */
+private abstract class AbstractBlob(private val algorithm: String) : Blob {
+    override val checksum: Checksum by lazy {
+        newInputStream().use { Checksum.fromInputStream(it, algorithm) }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Blob) return false
+        return checksum == other.checksum
+    }
+
+    override fun hashCode(): Int = Objects.hash(checksum)
 }
 
 /**
@@ -62,10 +80,8 @@ interface Blob {
          *
          * @throws [IOException] An I/O error occurred.
          */
-        fun fromFile(path: Path, algorithm: String): Blob = object : Blob {
+        fun fromFile(path: Path, algorithm: String): Blob = object : AbstractBlob(algorithm) {
             override fun newInputStream() = Files.newInputStream(path)
-
-            override val checksum: Checksum by lazy { Checksum.fromFile(path, algorithm) }
         }
 
         /**
@@ -86,14 +102,10 @@ interface Blob {
 
             // Iterate over each [blockSize] byte chunk of the file.
             for (position in 0 until fileSize step blockSize) {
-                val blob = object : Blob {
+                val blob = object : AbstractBlob(algorithm) {
                     override fun newInputStream(): InputStream = Files.newByteChannel(path).let {
                         it.position(position)
                         BoundedInputStream(Channels.newInputStream(it), blockSize)
-                    }
-
-                    override val checksum: Checksum by lazy {
-                        newInputStream().use { Checksum.fromInputStream(it, algorithm) }
                     }
                 }
 
@@ -110,16 +122,12 @@ interface Blob {
          *
          * @throws [IOException] An I/O error occurred.
          */
-        fun fromBlobs(blobs: Iterable<Blob>, algorithm: String): Blob = object : Blob {
+        fun fromBlobs(blobs: Iterable<Blob>, algorithm: String): Blob = object : AbstractBlob(algorithm) {
             // Lazily evaluate the input streams to avoid having too many open at once.
             override fun newInputStream(): InputStream = blobs
                 .asSequence()
                 .map { it.newInputStream() }
                 .let { SequenceInputStream(it.asEnumeration()) }
-
-            override val checksum: Checksum by lazy {
-                newInputStream().use { Checksum.fromInputStream(it, algorithm) }
-            }
         }
     }
 }
