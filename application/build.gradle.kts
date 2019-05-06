0710aa7d654428d8a3a70b31658532c302423440
Garrett Powell <garrett@gpowell.net>
2019-05-06 15:47:31 -0400

    Remove the ability to import/export repositories
    This commit removes the ability to import and export repositories as a
    file. The original purpose of the feature was to allow for backing up
    repositories. Now that repositories are stored in their working
    directories, they can be backed up with the working directory itself
    however the user chooses to do so.

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -12,11 +12,11 @@ dependencies {
     implementation(group = "org.jetbrains.exposed", name = "exposed", version = "0.13.6")
     implementation(group = "org.xerial", name = "sqlite-jdbc", version = "3.21.0.1")
     implementation(group = "com.github.ajalt", name = "clikt", version = "1.6.0")
-    implementation(group = "org.zeroturnaround", name = "zt-zip", version = "1.13")
     implementation(group = "com.google.code.gson", name = "gson", version = "2.8.5")
     implementation(group = "commons-io", name = "commons-io", version = "2.6")
     implementation(group = "ch.qos.logback", name = "logback-core", version = "1.2.3")
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
+
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.4.2")
 }
 
