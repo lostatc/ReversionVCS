52de8a967cc6f66f5ffa75151932a1a3d18e96fb
Garrett Powell <garrett@gpowell.net>
2019-05-09 15:37:18 -0400

    Set up JavaFX for implementing a GUI

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     application
+    id("org.openjfx.javafxplugin") version "0.0.7"
 }
 
 version = "0.1.0"
@@ -17,16 +18,30 @@ dependencies {
     implementation(group = "commons-io", name = "commons-io", version = "2.6")
     implementation(group = "ch.qos.logback", name = "logback-core", version = "1.2.3")
     implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
+    implementation(group = "com.jfoenix", name = "jfoenix", version = "9.0.8")
 
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.4.2")
 }
 
 application {
-    applicationName = "reversion"
-    mainClassName = "io.github.lostatc.reversion.cli.MainKt"
+    applicationName = "reversion-gui"
+    mainClassName = "io.github.lostatc.reversion.gui.MainKt"
+}
+
+
+javafx {
+    version = "12.0.1"
+    modules("javafx.fxml", "javafx.controls")
 }
 
 tasks {
+    register<CreateStartScripts>("cliScript") {
+        applicationName = "reversion"
+        mainClassName = "io.github.lostatc.reversion.cli.MainKt"
+        outputDir = getByName<CreateStartScripts>("startScripts").outputDir
+        classpath = getByName<CreateStartScripts>("startScripts").classpath
+    }
+
     register<CreateStartScripts>("daemonScript") {
         applicationName = "reversiond"
         mainClassName = "io.github.lostatc.reversion.daemon.MainKt"
