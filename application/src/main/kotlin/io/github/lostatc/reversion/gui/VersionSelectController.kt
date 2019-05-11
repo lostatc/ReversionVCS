4160a5b22b566e6338e6d6cfd6cc7a127070072b
Wren Powell <wrentpowell@gmail.com>
2019-05-11 17:11:52 -0400

    Show more information GUI
    Tags associated with versions and the time a version was created are now
    displayed for each version in the GUI.

--- application/src/main/kotlin/io/github/lostatc/reversion/gui/VersionSelectController.kt
+++ application/src/main/kotlin/io/github/lostatc/reversion/gui/VersionSelectController.kt
@@ -21,16 +21,26 @@ package io.github.lostatc.reversion.gui
 
 import com.jfoenix.controls.JFXListView
 import com.jfoenix.controls.JFXTextField
+import io.github.lostatc.reversion.api.Snapshot
+import io.github.lostatc.reversion.cli.format
 import io.github.lostatc.reversion.storage.WorkDirectory
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.Label
+import javafx.scene.layout.VBox
 import javafx.stage.FileChooser
 import java.net.URL
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.time.format.FormatStyle
 import java.util.ResourceBundle
+
+/**
+ * The name of the snapshot to display to the user.
+ */
+private val Snapshot.displayName: String
+    get() = tags.values.let { if (it.isEmpty()) "Version $revision" else it.joinToString { tag -> tag.name } }
 
 class VersionSelectController : Initializable {
 
@@ -44,7 +54,13 @@ class VersionSelectController : Initializable {
      * The list of versions in the UI.
      */
     @FXML
-    private lateinit var versionList: JFXListView<Label>
+    private lateinit var versionList: JFXListView<VBox>
+
+    /**
+     * The message to display if there are no versions.
+     */
+    @FXML
+    private lateinit var noVersionsMessage: VBox
 
     override fun initialize(location: URL, resources: ResourceBundle?) = Unit
 
@@ -53,8 +69,21 @@ class VersionSelectController : Initializable {
         val relativePath = workDirectory.path.relativize(path)
         val versions = workDirectory.timeline.listVersions(relativePath)
 
-        versionList.items.clear()
-        versionList.items.addAll(versions.map { Label("Version ${it.snapshot.revision}") })
+        noVersionsMessage.isVisible = false
+        versionList.isVisible = true
+
+        versionList.items.setAll(
+            versions.map {
+                VBox(
+                    Label(it.snapshot.displayName).apply {
+                        styleClass.add("card-title")
+                    },
+                    Label(it.snapshot.timeCreated.format(FormatStyle.MEDIUM)).apply {
+                        styleClass.add("card-subtitle")
+                    }
+                )
+            }
+        )
     }
 
     @FXML
