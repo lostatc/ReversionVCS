515bcd31441575ea14c86655cb83c73447bc3415
Wren Powell <wrentpowell@gmail.com>
2019-05-13 09:21:56 -0400

    Fix race conditions in GUI

--- application/src/main/kotlin/io/github/lostatc/reversion/gui/VersionSelectController.kt
+++ application/src/main/kotlin/io/github/lostatc/reversion/gui/VersionSelectController.kt
@@ -28,7 +28,8 @@ import io.github.lostatc.reversion.api.Tag
 import io.github.lostatc.reversion.api.Version
 import io.github.lostatc.reversion.cli.format
 import io.github.lostatc.reversion.storage.WorkDirectory
-import javafx.event.ActionEvent
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
@@ -48,7 +49,6 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.format.FormatStyle
 import java.util.ResourceBundle
-import kotlin.properties.Delegates
 
 /**
  * The tag to display in the UI, or `null` if there is none.
@@ -65,38 +65,19 @@ private val Snapshot.displayName: String
 class VersionSelectController : Initializable, CoroutineScope by MainScope() {
     /**
      * The versions currently visible in the UI.
-     *
-     * This sets the values in [versionList].
      */
-    private var versions: List<Version> = emptyList()
-        set(value) {
-            field = value
-
-            versionList.items.setAll(
-                value.map {
-                    VBox(
-                        Label(it.snapshot.displayName).apply {
-                            styleClass.add("card-title")
-                        },
-                        Label(it.snapshot.timeCreated.format(FormatStyle.MEDIUM)).apply {
-                            styleClass.add("card-subtitle")
-                        }
-                    )
-                }
-            )
-        }
+    private val versions: ObservableList<Version> = FXCollections.observableArrayList()
 
     /**
-     * The currently-selected version.
+     * The index of the currently selected version or `null` if there is none.
      */
-    private var selectedVersion: Version? by Delegates.observable<Version?>(null) { _, _, newValue ->
-        updateVersionInfo(newValue)
-    }
+    private var selectedIndex: Int? = null
 
     /**
-     * The index of the most recently selected version or -1 if there is none.
+     * The currently selected version.
      */
-    private var lastSelectedIndex: Int = -1
+    private val selectedVersion: Version?
+        get() = selectedIndex?.let { versions[it] }
 
     /**
      * The text field containing the path of the file to get versions for.
@@ -150,16 +131,27 @@ class VersionSelectController : Initializable, CoroutineScope by MainScope() {
         // Show a placeholder when there are no versions.
         versionList.placeholder = FXMLLoader.load(this::class.java.getResource("/fxml/NoVersionsPlaceholder.fxml"))
 
-        // Set the [selectedIndex] and [selectedVersion] when the user selects a version in the UI.
+        // Set the [selectedIndex] and update the UI when the user selects a version.
         versionList.selectionModel.selectedIndexProperty().addListener { _, _, newValue ->
-            val index = newValue.toInt()
+            // Save unsaved changes before loading.
+            saveVersionInfo()
 
-            if (index < 0) {
-                selectedVersion = null
-            } else {
-                lastSelectedIndex = index
-                selectedVersion = versions[index]
-            }
+            val index = newValue.toInt()
+            selectedIndex = if (index < 0) null else index
+
+            updateVersionInfo()
+        }
+
+        // Map [Version] objects in the model to [Node] objects to display in the view.
+        versionList.items = MappedList(versions) {
+            VBox(
+                Label(it.snapshot.displayName).apply {
+                    styleClass.add("card-title")
+                },
+                Label(it.snapshot.timeCreated.format(FormatStyle.MEDIUM)).apply {
+                    styleClass.add("card-subtitle")
+                }
+            )
         }
 
         // Make the version info invisible until a version is selected.
@@ -167,6 +159,7 @@ class VersionSelectController : Initializable, CoroutineScope by MainScope() {
 
         // Save the contents of the version description text area when it loses focus.
         versionDescriptionField.focusedProperty().addListener { _, _, focused ->
+            // TODO: Fix description not being updated when you click to another version.
             if (!focused) saveVersionInfo()
         }
     }
@@ -175,30 +168,21 @@ class VersionSelectController : Initializable, CoroutineScope by MainScope() {
      * Updates [versions] with the versions of the given [path].
      */
     private suspend fun loadVersions(path: Path) {
-        versions = withContext(Dispatchers.Default) {
+        val newVersions = withContext(Dispatchers.Default) {
             val workDirectory = WorkDirectory.openFromDescendant(path)
             val relativePath = workDirectory.path.relativize(path)
             workDirectory.timeline.listVersions(relativePath)
         }
+
+        versions.setAll(newVersions)
     }
 
     /**
-     * Updates [versions] with the current data from the repository.
+     * Update the UI to display information about the [selectedVersion].
      */
-    private suspend fun reloadVersions() {
-        val version = selectedVersion ?: return
+    private fun updateVersionInfo() {
+        val version = selectedVersion
 
-        versions = withContext(Dispatchers.Default) {
-            version.timeline.listVersions(version.path)
-        }
-
-        versionList.selectionModel.select(lastSelectedIndex)
-    }
-
-    /**
-     * Update the UI to display information about the given [version].
-     */
-    private fun updateVersionInfo(version: Version?) {
         if (version == null) {
             versionInfoPane.isVisible = false
             return
@@ -216,46 +200,48 @@ class VersionSelectController : Initializable, CoroutineScope by MainScope() {
      * Saves version information in the UI to storage.
      */
     @FXML
-    fun saveVersionInfo() = launch {
-        val version = selectedVersion ?: return@launch
+    fun saveVersionInfo() {
+        val index = selectedIndex ?: return
+        val version = selectedVersion ?: return
 
         val tagName = versionNameField.text
         val tagDescription = versionDescriptionField.text ?: ""
         val tagPinned = versionPinnedCheckBox.isSelected
 
-        withContext(Dispatchers.Default) {
-            val primaryTag = version.snapshot.primaryTag
+        launch {
+            withContext(Dispatchers.Default) {
+                version.snapshot.primaryTag?.apply {
+                    name = tagName
+                    description = tagDescription
+                    pinned = tagPinned
+                } ?: version.snapshot.addTag(name = tagName, description = tagDescription, pinned = tagPinned)
+            }
 
-            primaryTag?.apply {
-                name = tagName
-                description = tagDescription
-                pinned = tagPinned
-            } ?: version.snapshot.addTag(name = tagName, description = tagDescription, pinned = tagPinned)
+            versions[index] = version
         }
 
-        reloadVersions()
     }
 
     /**
      * Load versions by browsing for a file.
      */
     @FXML
-    fun browsePath(event: ActionEvent) = launch {
+    fun browsePath() {
         val file = FileChooser().run {
             title = "Select file"
-            showOpenDialog(pathField.scene.window)?.toPath() ?: return@launch
+            showOpenDialog(pathField.scene.window)?.toPath() ?: return
         }
 
         pathField.text = file.toString()
-        loadVersions(file)
+        launch { loadVersions(file) }
     }
 
     /**
      * Load versions by selecting a file path.
      */
     @FXML
-    fun setPath(event: ActionEvent) = launch {
+    fun setPath() {
         val file = Paths.get(pathField.text)
-        loadVersions(file)
+        launch { loadVersions(file) }
     }
 }
