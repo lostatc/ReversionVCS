7efe8562cbe81d5c58aa2c82230cf33fba653ce0
Wren Powell <wrentpowell@gmail.com>
2019-10-06 12:40:00 +0100

    Change how dialogs handle events

--- application/src/main/kotlin/io/github/lostatc/reversion/gui/Dialogs.kt
+++ application/src/main/kotlin/io/github/lostatc/reversion/gui/Dialogs.kt
@@ -26,9 +26,27 @@ import com.jfoenix.controls.JFXSpinner
 import io.github.lostatc.reversion.gui.controls.DateTimePicker
 import javafx.event.EventHandler
 import javafx.scene.control.Label
+import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
 import kotlinx.coroutines.Job
 import java.time.Instant
+
+/**
+ * A handler used to show a dialog and return a value based on a user selection.
+ */
+data class DialogHandle<T>(val dialog: JFXDialog, private val result: () -> T) {
+    fun show(pane: StackPane): T {
+        dialog.show(pane)
+        return result()
+    }
+
+    companion object {
+        /**
+         * Returns a new [DialogHandle] with a return type of [Unit].
+         */
+        fun of(dialog: JFXDialog): DialogHandle<Unit> = DialogHandle(dialog) { Unit }
+    }
+}
 
 /**
  * Creates a new dialog that presents the user with information and prompts them to dismiss it.
@@ -36,18 +54,22 @@ import java.time.Instant
  * @param [title] The title of the dialog.
  * @param [text] The message body of the dialog.
  */
-fun infoDialog(title: String, text: String): JFXDialog = JFXDialog().apply {
-    content = JFXDialogLayout().apply {
-        heading.add(Label(title))
-        body.add(Label(text))
-        actions.addAll(
-            JFXButton().apply {
-                this.text = "OK"
-                this.styleClass.addAll("button-text", "button-confirm")
-                this.onAction = EventHandler { close() }
-            }
-        )
+fun infoDialog(title: String, text: String): DialogHandle<Unit> {
+    val dialog = JFXDialog().apply {
+        content = JFXDialogLayout().apply {
+            heading.add(Label(title))
+            body.add(Label(text))
+            actions.addAll(
+                JFXButton().apply {
+                    this.text = "OK"
+                    this.styleClass.addAll("button-text", "button-confirm")
+                    this.onAction = EventHandler { close() }
+                }
+            )
+        }
     }
+
+    return DialogHandle.of(dialog)
 }
 
 /**
@@ -57,25 +79,34 @@ fun infoDialog(title: String, text: String): JFXDialog = JFXDialog().apply {
  *
  * @param [title] The title of the dialog.
  * @param [job] The job which this dialog is tied to.
+ *
+ * @return A handle which returns `true` if the job completed normally and `false` if it was cancelled.
  */
-fun processingDialog(title: String, job: Job): JFXDialog = JFXDialog().apply {
-    isOverlayClose = false
-    content = JFXDialogLayout().apply {
-        heading.add(Label(title))
-        body.add(JFXSpinner())
-        actions.addAll(
-            JFXButton().apply {
-                this.text = "Cancel"
-                this.styleClass.addAll("button-text", "button-regular")
-                this.onAction = EventHandler {
-                    close()
-                    job.cancel()
+fun processingDialog(title: String, job: Job): DialogHandle<Boolean> {
+    var finished = true
+
+    val dialog = JFXDialog().apply {
+        isOverlayClose = false
+        content = JFXDialogLayout().apply {
+            heading.add(Label(title))
+            body.add(JFXSpinner())
+            actions.addAll(
+                JFXButton().apply {
+                    this.text = "Cancel"
+                    this.styleClass.addAll("button-text", "button-regular")
+                    this.onAction = EventHandler {
+                        job.cancel()
+                        finished = false
+                        close()
+                    }
                 }
-            }
-        )
+            )
+        }
+
+        job.invokeOnCompletion { close() }
     }
 
-    job.invokeOnCompletion { close() }
+    return DialogHandle(dialog) { finished }
 }
 
 /**
@@ -83,28 +114,37 @@ fun processingDialog(title: String, job: Job): JFXDialog = JFXDialog().apply {
  *
  * @param [title] The title of the dialog.
  * @param [text] The message body of the dialog.
- * @param [action] The action to perform if the user confirms.
+ *
+ * @return A handle which returns whether the user confirmed the action.
  */
-fun confirmationDialog(title: String, text: String, action: () -> Unit): JFXDialog = JFXDialog().apply {
-    content = JFXDialogLayout().apply {
-        heading.add(Label(title))
-        body.add(Label(text))
-        actions.addAll(
-            JFXButton().apply {
-                this.text = "No"
-                this.styleClass.addAll("button-text", "button-regular")
-                this.onAction = EventHandler { close() }
-            },
-            JFXButton().apply {
-                this.text = "Yes"
-                this.styleClass.addAll("button-text", "button-confirm")
-                this.onAction = EventHandler {
-                    close()
-                    action()
+fun confirmationDialog(title: String, text: String): DialogHandle<Boolean> {
+    var confirmed = false
+
+    val dialog = JFXDialog().apply {
+        content = JFXDialogLayout().apply {
+            heading.add(Label(title))
+            body.add(Label(text))
+            actions.addAll(
+                JFXButton().apply {
+                    this.text = "No"
+                    this.styleClass.addAll("button-text", "button-regular")
+                    this.onAction = EventHandler {
+                        close()
+                    }
+                },
+                JFXButton().apply {
+                    this.text = "Yes"
+                    this.styleClass.addAll("button-text", "button-confirm")
+                    this.onAction = EventHandler {
+                        confirmed = true
+                        close()
+                    }
                 }
-            }
-        )
+            )
+        }
     }
+
+    return DialogHandle(dialog) { confirmed }
 }
 
 /**
@@ -112,28 +152,35 @@ fun confirmationDialog(title: String, text: String, action: () -> Unit): JFXDial
  *
  * @param [title] The title of the dialog.
  * @param [text] The message body of the dialog.
- * @param [action] The action to perform if the user confirms.
+ *
+ * @return A handle which returns whether the user approved the action.
  */
-fun approvalDialog(title: String, text: String, action: () -> Unit): JFXDialog = JFXDialog().apply {
-    content = JFXDialogLayout().apply {
-        heading.add(Label(title))
-        body.add(Label(text))
-        actions.addAll(
-            JFXButton().apply {
-                this.text = "No"
-                this.styleClass.addAll("button-text", "button-regular")
-                this.onAction = EventHandler { close() }
-            },
-            JFXButton().apply {
-                this.text = "Yes"
-                this.styleClass.addAll("button-text", "button-danger")
-                this.onAction = EventHandler {
-                    close()
-                    action()
+fun approvalDialog(title: String, text: String): DialogHandle<Boolean> {
+    var approved = false
+
+    val dialog = JFXDialog().apply {
+        content = JFXDialogLayout().apply {
+            heading.add(Label(title))
+            body.add(Label(text))
+            actions.addAll(
+                JFXButton().apply {
+                    this.text = "No"
+                    this.styleClass.addAll("button-text", "button-regular")
+                    this.onAction = EventHandler { close() }
+                },
+                JFXButton().apply {
+                    this.text = "Yes"
+                    this.styleClass.addAll("button-text", "button-danger")
+                    this.onAction = EventHandler {
+                        approved = true
+                        close()
+                    }
                 }
-            }
-        )
+            )
+        }
     }
+
+    return DialogHandle(dialog) { approved }
 }
 
 /**
@@ -142,41 +189,47 @@ fun approvalDialog(title: String, text: String, action: () -> Unit): JFXDialog =
  * @param [title] The title of the dialog.
  * @param [text] The message body of the dialog.
  * @param [default] The default date and time that is selected in the dialog.
- * @param [action] The action to perform when the user confirms their selection.
+ *
+ * @return A handle which returns the date/time chosen by the user or `null` if none was chosen.
  */
 fun dateTimeDialog(
     title: String,
     text: String,
-    default: Instant? = null,
-    action: (Instant?) -> Unit
-): JFXDialog = JFXDialog().apply {
-    content = JFXDialogLayout().apply {
-        val dateTimePicker = DateTimePicker().apply {
-            instant = default
-        }
+    default: Instant? = null
+): DialogHandle<Instant?> {
+    var result: Instant? = null
 
-        heading.add(Label(title))
-
-        body.add(
-            VBox(Label(text), dateTimePicker).apply {
-                spacing = 15.0
+    val dialog = JFXDialog().apply {
+        content = JFXDialogLayout().apply {
+            val dateTimePicker = DateTimePicker().apply {
+                instant = default
             }
-        )
 
-        actions.addAll(
-            JFXButton().apply {
-                this.text = "Cancel"
-                this.styleClass.addAll("button-text", "button-regular")
-                this.onAction = EventHandler { close() }
-            },
-            JFXButton().apply {
-                this.text = "OK"
-                this.styleClass.addAll("button-text", "button-confirm")
-                this.onAction = EventHandler {
-                    close()
-                    action(dateTimePicker.instant)
+            heading.add(Label(title))
+
+            body.add(
+                VBox(Label(text), dateTimePicker).apply {
+                    spacing = 15.0
                 }
-            }
-        )
+            )
+
+            actions.addAll(
+                JFXButton().apply {
+                    this.text = "Cancel"
+                    this.styleClass.addAll("button-text", "button-regular")
+                    this.onAction = EventHandler { close() }
+                },
+                JFXButton().apply {
+                    this.text = "OK"
+                    this.styleClass.addAll("button-text", "button-confirm")
+                    this.onAction = EventHandler {
+                        result = dateTimePicker.instant
+                        close()
+                    }
+                }
+            )
+        }
     }
+
+    return DialogHandle(dialog) { result }
 }
