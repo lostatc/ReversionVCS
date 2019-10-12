b66a9f393269c69c39a9a765b867889201d5ac70
Wren Powell <wrentpowell@gmail.com>
2019-10-12 08:35:03 +0100

    Simplify loading of FXML files

--- application/src/main/kotlin/io/github/lostatc/reversion/gui/FormattingUtils.kt
+++ application/src/main/kotlin/io/github/lostatc/reversion/gui/FormattingUtils.kt
@@ -17,7 +17,7 @@
  * along with Reversion.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package io.github.lostatc.reversion.gui.models
+package io.github.lostatc.reversion.gui
 
 import java.time.Instant
 import java.time.ZoneId
